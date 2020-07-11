16af05c9d27c1d1f57498ea4e4c22ca55acc0944
Самсонова Мария Андреевна <samsonova.mariya2@wildberries.ru>
2020-07-11 13:55:24 +0300

    Initial commit

---
+++ app/src/main/java/ru/skillbranch/gameofthrones/MainActivity.kt
@@ -0,0 +1,18 @@
+package ru.skillbranch.gameofthrones
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.skillbranch.gameofthrones.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
