4648134e59a5005d9e51d4adf9f49492949e23bf
Самсонова Мария Андреевна <samsonova.mariya2@wildberries.ru>
2020-07-15 10:05:57 +0300

    - draft of ColoredTabsAppBar. Bugged version

--- app/src/main/java/ru/skillbranch/gameofthrones/ui/main/MainFragment.kt
+++ app/src/main/java/ru/skillbranch/gameofthrones/ui/main/MainFragment.kt
@@ -1,33 +1,34 @@
 package ru.skillbranch.gameofthrones.ui.main
 
-import android.animation.Animator
-import android.animation.AnimatorListenerAdapter
-import android.animation.ArgbEvaluator
-import android.graphics.Rect
 import android.os.Bundle
 import android.view.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
-import com.google.android.material.appbar.AppBarLayout
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import org.koin.android.ext.android.getKoin
 import org.koin.android.viewmodel.koin.getViewModel
-import ru.skillbranch.gameofthrones.AppConfig
 import ru.skillbranch.gameofthrones.HouseType
 import ru.skillbranch.gameofthrones.R
 import ru.skillbranch.gameofthrones.databinding.FragmentMainBinding
 import ru.skillbranch.gameofthrones.ui.characters.list.CharactersListFragment
-import kotlin.math.max
+import ru.skillbranch.gameofthrones.utils.views.ColoredTabsAppBar
 
 class MainFragment : Fragment(), SearchView.OnQueryTextListener {
 
     private lateinit var viewModel: MainViewModel
     private lateinit var vb: FragmentMainBinding
+
+    val toolbar: Toolbar
+        get() = vb.tabsAppBar.toolbar
+
+    val tabLayout: TabLayout
+        get() = vb.tabsAppBar.tabLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,27 +44,11 @@ class MainFragment : Fragment(), SearchView.OnQueryTextListener {
             .root
     }
 
-    private fun initToolbarDimension() {
-        val params =
-            vb.toolbar.getLayoutParams() as AppBarLayout.LayoutParams
-        params.setMargins(0, getStatusBarHeight(), 0, 0)
-        vb.toolbar.setLayoutParams(params)
-    }
-
-    fun getStatusBarHeight(): Int {
-        var result = 0
-        val resourceId = resources.getIdentifier("status_bar_height", "dimen", "android")
-        if (resourceId > 0) {
-            result = resources.getDimensionPixelSize(resourceId)
-        }
-        return result
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initToolbarDimension()
 
-        (requireActivity() as AppCompatActivity).setSupportActionBar(vb.toolbar)
+        (requireActivity() as AppCompatActivity).setSupportActionBar(toolbar)
         vb.viewPager.adapter = ViewPagerFragmentStateAdapter(requireActivity())
         vb.viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
 
@@ -72,18 +57,18 @@ class MainFragment : Fragment(), SearchView.OnQueryTextListener {
                 positionOffset: Float,
                 positionOffsetPixels: Int
             ) {
-           //     vb.backgroundView.setBackgroundColor(getBackgroundColor(position, positionOffset))
+
             }
 
             override fun onPageScrollStateChanged(state: Int) {
-               if (state == ViewPager2.SCROLL_STATE_SETTLING) {
-                    val position = vb.viewPager.currentItem
+                if (state == ViewPager2.SCROLL_STATE_SETTLING) {
+                    /*val position = vb.viewPager.currentItem
                     val currentThemeId = AppConfig.appThemeId
                     val newThemeId = if (position == 0) R.style.AppThemeOverlay_Lannister else R.style.AppThemeOverlay_Baratheon
                     if (currentThemeId != newThemeId) {
                         AppConfig.appThemeId = newThemeId
-                        vb.toolbar.context.setTheme(newThemeId)
-                    }
+                        toolbar.context.setTheme(newThemeId)
+                    }*/
                 }
             }
 
@@ -91,25 +76,23 @@ class MainFragment : Fragment(), SearchView.OnQueryTextListener {
 
             }
         })
-        vb.tabs.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
             override fun onTabReselected(tab: TabLayout.Tab?) {}
 
             override fun onTabUnselected(tab: TabLayout.Tab?) {}
 
             override fun onTabSelected(tab: TabLayout.Tab?) {
-                tab?.let {
-                    if (it.position > 0) {
-                        val rectf = Rect()
-                        it.view.getGlobalVisibleRect(rectf)
-                        reveal(it.position, rectf.exactCenterX(), rectf.exactCenterY())
-                    }
-                }
+
             }
 
         })
-        TabLayoutMediator(vb.tabs, vb.viewPager) { tab, position ->
-            tab.text = HouseType.values()[position].shortName //FIXME: to VM
-        }.attach()
+        vb.tabsAppBar.colorIds = HouseType.values().map { it.colorPrimaryId }
+        ColoredTabsAppBar.ColoredTabsAppBarMediator(
+            vb.tabsAppBar,
+            vb.viewPager,
+            TabLayoutMediator.TabConfigurationStrategy { tab, position ->
+                tab.text = HouseType.values()[position].shortName
+            }).attach()
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -119,12 +102,12 @@ class MainFragment : Fragment(), SearchView.OnQueryTextListener {
 
     override fun onResume() {
         super.onResume()
-       /* val listener = object: View.OnAttachStateChangeListener {
-            override fun onViewAttachedToWindow(v: View?) {
-            }
-            override fun onViewDetachedFromWindow(v: View?) {}
-        }
-        vb.mainContainer.addOnAttachStateChangeListener(listener) */
+        /* val listener = object: View.OnAttachStateChangeListener {
+             override fun onViewAttachedToWindow(v: View?) {
+             }
+             override fun onViewDetachedFromWindow(v: View?) {}
+         }
+         vb.mainContainer.addOnAttachStateChangeListener(listener) */
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -171,43 +154,5 @@ class MainFragment : Fragment(), SearchView.OnQueryTextListener {
         override fun getItemCount(): Int = HouseType.values().size
 
     }
-
-    fun reveal(position: Int, x: Float, y: Float) {
-        val houseType = HouseType.values()[position]
-        vb.revealView.setVisibility(View.VISIBLE)
-        val revealViewX: Int = vb.revealView.getWidth()
-        val revealViewY: Int = vb.revealView.getHeight()
-        val radius = max(revealViewX, revealViewY) * 1.2f
-        val reveal = ViewAnimationUtils
-            .createCircularReveal(vb.revealView, x.toInt(), y.toInt(), 0f, radius)
-        reveal.duration = 2000
-        val newBackgroundColor = getColor(houseType.colorPrimaryId)
-        reveal.addListener(object : AnimatorListenerAdapter() {
-            override fun onAnimationEnd(animator: Animator) {
-                vb.backgroundView.setBackgroundColor(newBackgroundColor)
-                vb.revealView.setVisibility(View.INVISIBLE)
-            }
-        })
-        vb.revealView.setBackgroundColor(newBackgroundColor)
-        reveal.start()
-    }
-
-    private fun getBackgroundColor(position: Int, positionOffset: Float): Int {
-        val houseType = HouseType.values()[position]
-        val nextHouseType = HouseType.values()[ position +
-        when {
-            positionOffset == 0f -> 0
-            positionOffset > 0 -> 1
-            else -> -1
-        }]
-        val startColor: Int = getColor(houseType.colorPrimaryId)
-        val endColor: Int = getColor(nextHouseType.colorPrimaryId)
-        val argbEvaluator = ArgbEvaluator()
-        return argbEvaluator.evaluate(positionOffset, startColor, endColor) as Int
-    }
-
-    private fun getTheme() = requireActivity().theme
-
-    private fun getColor(colorId: Int) = resources.getColor(colorId, getTheme())
 
 }
---
+++ app/src/main/java/ru/skillbranch/gameofthrones/utils/views/ColoredTabsAppBar.kt
@@ -0,0 +1,175 @@
+package ru.skillbranch.gameofthrones.utils.views
+
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ArgbEvaluator
+import android.content.Context
+import android.graphics.Rect
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.MotionEvent
+import android.view.View
+import android.view.ViewAnimationUtils
+import androidx.appcompat.widget.Toolbar
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.appbar.AppBarLayout
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import ru.skillbranch.gameofthrones.databinding.ViewColoredTabsAppBarBinding
+import kotlin.math.max
+
+
+class ColoredTabsAppBar(context: Context, attrs: AttributeSet) : ConstraintLayout(context, attrs) {
+
+    var colorIds: List<Int> = emptyList()
+    val toolbar: Toolbar
+        get() = vb.toolbar
+
+    val tabLayout: TabLayout
+        get() = vb.tabs
+
+    private val vb = ViewColoredTabsAppBarBinding.inflate(LayoutInflater.from(context), this)
+    private var lastMotionEvent: MotionEvent? = null
+    private var lastAnimation: Animator? = null
+
+    init {
+        initToolbarDimension()
+
+    }
+
+    override fun dispatchTouchEvent(event: MotionEvent?): Boolean {
+        when (event?.action) {
+            MotionEvent.ACTION_UP -> {
+                //TODO: только если событие принадлежит tabLayout
+                lastMotionEvent = event
+            }
+        }
+        return super.dispatchTouchEvent(event)
+    }
+
+    // fun animateBackground(position: Int, positionOffset: Float, only )
+
+    fun animateTabChangeOnlyOnEvent(position: Int) {
+        lastMotionEvent?.let {
+            //animateTabChange(position, it.rawX, it.rawY)
+            animateTabChange(position)
+        }
+        lastMotionEvent = null
+    }
+
+    fun animateTabChange(
+        position: Int,
+        animationStartX: Float? = null,
+        animationStartY: Float? = null
+    ) {
+        val tab = checkNotNull(vb.tabs.getTabAt(position))
+        val rect = Rect()
+        tab.view.getGlobalVisibleRect(rect)
+        val newBackgroundColor = checkNotNull(getColorAtPosition(position))
+        lastAnimation = with(vb.revealView) {
+            visibility = View.VISIBLE
+            setBackgroundColor(newBackgroundColor)
+            val radius = max(width, height) * 1.2f
+            val startX = (animationStartX ?: rect.exactCenterX()).toInt()
+            val startY = (animationStartY ?: rect.exactCenterY()).toInt()
+            ViewAnimationUtils.createCircularReveal(this, startX, startY, 0f, radius)
+                .apply {
+                    duration = 2000
+                    addListener(object : AnimatorListenerAdapter() {
+                        override fun onAnimationEnd(animator: Animator) {
+                            vb.backgroundView.setBackgroundColor(newBackgroundColor)
+                            visibility = View.INVISIBLE
+                            lastAnimation = null
+                        }
+                    })
+                }
+        }.also {
+            it.start()
+        }
+    }
+
+    private fun getBackgroundColor(position: Int, positionOffset: Float): Int? {
+        val nextPosition = position +
+                when {
+                    positionOffset == 0f -> 0
+                    positionOffset > 0 -> 1
+                    else -> -1
+                }
+        val startColor = getColorAtPosition(position)
+        val endColor = getColorAtPosition(nextPosition)
+        val argbEvaluator = ArgbEvaluator()
+        return if (startColor != null && endColor != null) {
+            when (startColor) {
+                endColor -> startColor
+                else -> argbEvaluator.evaluate(positionOffset, startColor, endColor) as Int
+            }
+        } else {
+            null
+        }
+    }
+
+    private fun getColorAtPosition(position: Int): Int? {
+        val colorId = if (colorIds.size > position) colorIds[position] else null
+        return colorId?.let { ContextCompat.getColor(context, it) }
+    }
+
+    //FIXME: лучше, наверное, вынести в CustomAppBarLayout
+    private fun initToolbarDimension() {
+        val params = toolbar.layoutParams as AppBarLayout.LayoutParams
+        params.setMargins(0, getStatusBarHeight(), 0, 0)
+        toolbar.layoutParams = params
+    }
+
+    //FIXME: погуглить более надёжный способ
+    private fun getStatusBarHeight(): Int {
+        var result = 0
+        val resourceId = resources.getIdentifier("status_bar_height", "dimen", "android")
+        if (resourceId > 0) {
+            result = resources.getDimensionPixelSize(resourceId)
+        }
+        return result
+    }
+
+    class ColoredTabsAppBarMediator(
+        private val tabsAppBar: ColoredTabsAppBar,
+        private val viewPager: ViewPager2,
+        tabConfigurationStrategy: TabLayoutMediator.TabConfigurationStrategy
+    ) {
+        val tabMediator =
+            TabLayoutMediator(tabsAppBar.tabLayout, viewPager, tabConfigurationStrategy)
+        var onPageSelected: ((position: Int) -> Unit)? = null
+
+        fun attach() {
+            viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+
+                override fun onPageScrolled(
+                    position: Int,
+                    positionOffset: Float,
+                    positionOffsetPixels: Int
+                ) {
+                    with(tabsAppBar) {
+                        if (lastMotionEvent == null && lastAnimation == null) {
+                            getBackgroundColor(position, positionOffset)?.let {
+                                vb.backgroundView.setBackgroundColor(it)
+                            }
+                        }
+                    }
+                }
+
+                override fun onPageScrollStateChanged(state: Int) {
+                    if (state == ViewPager2.SCROLL_STATE_IDLE) {
+                        tabsAppBar.animateTabChangeOnlyOnEvent(tabsAppBar.tabLayout.selectedTabPosition)
+                    }
+                }
+
+                override fun onPageSelected(position: Int) {
+                    //tabsAppBar.animateTabChange(position, onlyOnEvent = true)
+                }
+            })
+            tabMediator.attach()
+        }
+    }
+
+}
